50e7c5a449a937448bdf42885e0d49017656713b
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2025-06-05 10:42:37 -0300

    Alterando a solução da busca por id.

--- src/main/java/br/edu/ifsp/arqdsw2/taskAPI/controller/FrontController.java
+++ src/main/java/br/edu/ifsp/arqdsw2/taskAPI/controller/FrontController.java
@@ -1,15 +1,9 @@
 package br.edu.ifsp.arqdsw2.taskAPI.controller;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
-import br.edu.ifsp.arqdsw2.taskAPI.controller.command.AtualizarTarefaCommand;
 import br.edu.ifsp.arqdsw2.taskAPI.controller.command.Command;
-import br.edu.ifsp.arqdsw2.taskAPI.controller.command.CriarTarefaCommand;
-import br.edu.ifsp.arqdsw2.taskAPI.controller.command.DeletarTarefaCommand;
-import br.edu.ifsp.arqdsw2.taskAPI.controller.command.ListarTarefasCommand;
-import br.edu.ifsp.arqdsw2.taskAPI.controller.command.RecuperarTarefaCommand;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -17,32 +11,22 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/tarefas/*")
 public class FrontController extends HttpServlet {
-	private final Map<String, Command> comandos = new HashMap<>();
+	private TarefaDispatcher dispatcher;
 
 	@Override
 	public void init() {
-		comandos.put("POST", new CriarTarefaCommand());
-		comandos.put("PUT", new AtualizarTarefaCommand());
-		comandos.put("DELETE", new DeletarTarefaCommand());
+		dispatcher = new TarefaDispatcher();
 	}
 
 	@Override
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		try {
-			String metodo = request.getMethod();
-			Command comando = comandos.get(metodo);
+			Command comando = dispatcher.resolver(request);
 			if (comando != null) {
 				comando.executar(request, response);
-			} else if(metodo.equals("GET")){
-				String id = request.getPathInfo();
-				if(id!=null) {
-					comando = new RecuperarTarefaCommand();
-				}else {
-					comando = new ListarTarefasCommand();
-				}
-				comando.executar(request, response);
-			}else {
-				response.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+			} else {
+				response.sendError(HttpServletResponse.SC_NOT_FOUND, "Rota não encontrada.");
 			}
 		} catch (Exception e) {
 			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
---
+++ src/main/java/br/edu/ifsp/arqdsw2/taskAPI/controller/TarefaDispatcher.java
@@ -0,0 +1,35 @@
+package br.edu.ifsp.arqdsw2.taskAPI.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import br.edu.ifsp.arqdsw2.taskAPI.controller.command.AtualizarTarefaCommand;
+import br.edu.ifsp.arqdsw2.taskAPI.controller.command.Command;
+import br.edu.ifsp.arqdsw2.taskAPI.controller.command.CriarTarefaCommand;
+import br.edu.ifsp.arqdsw2.taskAPI.controller.command.DeletarTarefaCommand;
+import br.edu.ifsp.arqdsw2.taskAPI.controller.command.ListarTarefasCommand;
+import br.edu.ifsp.arqdsw2.taskAPI.controller.command.RecuperarTarefaCommand;
+import jakarta.servlet.http.HttpServletRequest;
+
+public class TarefaDispatcher {
+	private final Map<String, Command> rotas = new HashMap<>();
+
+	public TarefaDispatcher() {
+		rotas.put("GET:/", new ListarTarefasCommand());
+		rotas.put("POST:/", new CriarTarefaCommand());
+		rotas.put("GET:/id", new RecuperarTarefaCommand());
+		rotas.put("PUT:/id", new AtualizarTarefaCommand());
+		rotas.put("DELETE:/id", new DeletarTarefaCommand());
+	}
+
+	public Command resolver(HttpServletRequest request) {
+		String method = request.getMethod();
+		String path = request.getPathInfo();
+		if (path == null || path.equals("/")) {
+			return rotas.get(method + ":/");
+		} else if (path.matches("^/\\d+$")) {
+			return rotas.get(method + ":/id");
+		}
+		return null;
+	}
+}
